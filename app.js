d7cb751f2369c7b11ca59cde76053692ac2374c6
Ben Chen <chenben1996@gmail.com>
2021-04-12 13:32:03 -0500

    test

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ mongoose.connect('mongodb://localhost:27017/yelp-camp', {
     useFindAndModify: false
 });
 
+
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", () => {
